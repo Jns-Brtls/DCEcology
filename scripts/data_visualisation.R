2516e3cd10c281063a7239fefa05ec8bd30e5108
Jns-Brtls <jonas.bertels@uantwerpen.be>
2018-06-13 12:33:25 +0200

    first plot created succesfully

--- scripts/data_visualisation.R
+++ scripts/data_visualisation.R
@@ -4,3 +4,10 @@ library(tidyverse)
 download.file("https://ndownloader.figshare.com/files/11930600?private_link=fe0cd1848e06456e6f38",
               "data/surveys_complete.csv")
 surveys_complete <- read_csv("data/surveys_complete.csv")
+
+ggplot(data = surveys_complete)
+
+ggplot(data = surveys_complete, aes(x = weight, y = hindfoot_length))
+
+ggplot(data = surveys_complete, aes(x = weight, y = hindfoot_length)) +
+  geom_point()
