ff310c23ffcaca5fb39549f0f28d95cf19e45c79
Jns-Brtls <jonas.bertels@uantwerpen.be>
2018-06-13 14:47:31 +0200

    up until saved plot

--- scripts/data_visualisation.R
+++ scripts/data_visualisation.R
@@ -47,6 +47,13 @@ ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
 # of points) is drawn. Replace the box plot with a violin plot; see
 # geom_violin().
 
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_violin() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(title = "Species and weight\n", x = "Species", y = "Weight", color = "Plot id\n") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
 # In many types of data, it is important to consider the scale of the
 # observations. For example, it may be worth changing the scale of the axis to
 # better distribute the observations in the space of the plot. Changing the
@@ -54,13 +61,77 @@ ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
 # (i.e., by incrementally adding commands). Try making these modifications:
 # Represent weight on the log10 scale; see scale_y_log10().
 
+ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
+  geom_violin() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(title = "Species and weight\n", x = "Species", y = "Weight", color = "Plot id\n") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  scale_y_log10()
+
 # So far, we’ve looked at the distribution of weight within species. Try making
 # a new plot to explore the distribution of another variable within each
 # species. Create a boxplot for hindfoot_length. Overlay the boxplot layer on a
 # jitter layer to show actual measurements. Add color to the data points on your
 # boxplot according to the plot from which the sample was taken (plot_id).
 
+ggplot(data = surveys_complete, aes(x = species_id, y = hindfoot_length)) +
+  geom_jitter(alpha = 0.15, aes(color = plot_id)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(title = "Species and weight\n", x = "Species", y = "Weight", color = "Plot id\n") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
 # Hint: Check the class for plot_id. Consider changing the class of plot_id from
 # integer to factor. Why does this change how R makes the graph?
   
+yearly_count <- surveys_complete %>% 
+  group_by(year, species_id) %>% 
+  tally()
+
+ggplot(data = yearly_count, aes(x = year, y = n, group =  species_id)) +
+  geom_line()
+
+ggplot(data = yearly_count, aes(x = year, y = n, color =  species_id)) +
+  geom_line()
+
+ggplot(data = yearly_count, aes(x = year, y = n, color =  species_id)) +
+  geom_line() +
+  facet_wrap(~ species_id)
+
+yearly_sex_counts <- surveys_complete %>% 
+  group_by(year, species_id, sex) %>% 
+  tally()
+
+ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        text = element_text(size = 12),
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(title = "Observations over time", x = "Year of observation", y = "Number of observations", color = "Sex")
+
+grey_theme <-   theme(panel.grid = element_blank(),
+                      text = element_text(size = 12),
+                      axis.text.x = element_text(angle = 45, hjust = 1))
+  
+ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  theme_bw() +
+  grey_theme +
+  labs(title = "Observations over time", x = "Year of observation", y = "Number of observations", color = "Sex")
+
+my_plot <- ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  theme_bw() +
+  grey_theme +
+  labs(title = "Observations over time", x = "Year of observation", y = "Number of observations", color = "Sex")
+
+ggsave("plots/my_first_plot.png", my_plot, width = 15, height = 10, units = "cm", dpi = 300)
+
+
 
